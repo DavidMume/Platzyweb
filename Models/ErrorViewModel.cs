97af0f0801e478703cea406388a593be8f21c961
davidmume <juan.mu.me@hotmail.com>
2025-05-26 15:46:03 +1000

    Initial commit: clean .NET 8 app with Azure deploy workflow

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatzyWebClean.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
